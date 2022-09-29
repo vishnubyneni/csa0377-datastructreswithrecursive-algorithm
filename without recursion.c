70b7c3b91f2b5efecadc59aa9800f8030691a202
vishnubyneni <113878106+vishnubyneni@users.noreply.github.com>
2022-09-29 20:41:08 +0530

    Add files via upload

---
+++ without recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int n,i,fact=1;
+printf("Enter a number to find factorial: ");
+scanf("%d",&n);
+for(i=1;i<=n;++i)
+{
+fact=fact*i;       
+}
+printf("Factorial of Number %d =%d",n,fact);
+}
+
